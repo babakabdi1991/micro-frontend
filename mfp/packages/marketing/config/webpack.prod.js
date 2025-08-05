45ba11e2d7a272eb2e9269bcadb55fded81e2335
babak.abdi@zoodfood.com <babak.abdi@zoodfood.com>
2025-08-05 14:04:10 +0330

    add production settings

---
+++ mfp/packages/marketing/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+const { ModuleFederationPlugin } = require("webpack").container;
+const packageJson = require("../package.json");
+
+const prodConfig = {
+  mode: "production",
+  output:{
+    filename:"[name].[contenthash].js"
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Marketing": "./src/bootstrap.js",
+      },
+      shared: packageJson.dependencies,
+    }),
+   
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
